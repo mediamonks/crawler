1380cb1f64b03714c6589ee305b7fc01ad2a92ca
Robert <robertslootjes@gmail.com>
2016-11-25 13:00:51 +0100

    - Added more tests for crawling

--- tests/src/CrawlerTest.php
+++ tests/src/CrawlerTest.php
@@ -3,6 +3,7 @@
 namespace tests\MediaMonks\Crawler;
 
 use MediaMonks\Crawler\Crawler;
+use MediaMonks\Crawler\Url\Matcher\PathRegexUrlMatcher;
 use MediaMonks\Crawler\Url\Matcher\UrlMatcherInterface;
 use Mockery as m;
 use Psr\Log\NullLogger;
@@ -72,6 +73,36 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(1, $crawler->getBlacklistUrlMatchers());
     }
 
+    public function test_clear_matchers()
+    {
+        $matcher = m::mock(UrlMatcherInterface::class);
+
+        $crawler = new Crawler();
+        $crawler->addBlacklistUrlMatcher($matcher);
+        $crawler->addWhitelistUrlMatcher($matcher);
+
+        $this->assertCount(1, $crawler->getBlacklistUrlMatchers());
+        $this->assertCount(1, $crawler->getWhitelistUrlMatchers());
+
+        $crawler->clearBlacklistUrlMatchers();
+        $crawler->clearWhitelistUrlMatchers();
+
+        $this->assertCount(0, $crawler->getBlacklistUrlMatchers());
+        $this->assertCount(0, $crawler->getWhitelistUrlMatchers());
+
+        $crawler->setBlacklistUrlMatchers([$matcher]);
+        $crawler->setWhitelistUrlMatchers([$matcher]);
+
+        $this->assertCount(1, $crawler->getBlacklistUrlMatchers());
+        $this->assertCount(1, $crawler->getWhitelistUrlMatchers());
+
+        $crawler->clearBlacklistUrlMatchers();
+        $crawler->clearWhitelistUrlMatchers();
+
+        $this->assertCount(0, $crawler->getBlacklistUrlMatchers());
+        $this->assertCount(0, $crawler->getWhitelistUrlMatchers());
+    }
+
     public function test_crawl_single_page()
     {
         $domCrawler = new DomCrawler('<html></html>');
@@ -89,6 +120,57 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
 
     public function test_crawl_multiple_pages()
     {
+        $crawler = new Crawler($this->getDummyClient());
+
+        foreach ($crawler->crawl('http://my-test') as $page) {
+        }
+
+        $this->assertCount(5, $crawler->getUrlsCrawled());
+        $this->assertCount(5, $crawler->getUrlsReturned());
+        $this->assertCount(2, $crawler->getUrlsRejected());
+    }
+
+    public function test_crawl_with_limit()
+    {
+        $crawler = new Crawler($this->getDummyClient());
+        $crawler->setLimit(3);
+
+        foreach ($crawler->crawl('http://my-test') as $page) {
+        }
+
+        $this->assertCount(3, $crawler->getUrlsCrawled());
+        $this->assertCount(2, $crawler->getUrlsQueued());
+    }
+
+    public function test_crawl_with_whitelist()
+    {
+        $crawler = new Crawler($this->getDummyClient());
+        $crawler->addWhitelistUrlMatcher(new PathRegexUrlMatcher('~^/page_1.html~'));
+
+        foreach ($crawler->crawl('http://my-test') as $page) {
+        }
+
+        $this->assertCount(5, $crawler->getUrlsCrawled());
+        $this->assertCount(1, $crawler->getUrlsReturned());
+    }
+
+    public function test_crawl_with_blacklist()
+    {
+        $crawler = new Crawler($this->getDummyClient());
+        $crawler->addBlacklistUrlMatcher(new PathRegexUrlMatcher('~^/page_1.html~'));
+
+        foreach ($crawler->crawl('http://my-test') as $page) {
+        }
+
+        $this->assertCount(5, $crawler->getUrlsCrawled());
+        $this->assertCount(4, $crawler->getUrlsReturned());
+    }
+
+    /**
+     * @return m\MockInterface
+     */
+    protected function getDummyClient()
+    {
         $client = m::mock(Client::class);
 
         $i = 0;
@@ -99,25 +181,20 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
                     $html = '<html><body><a href="/page_1.html">Page 1</a><a href="/page_2.html">Page 2</a></body></html>';
                     break;
                 case 2:
-                    $html = '<html><body><a href="/page_3.html">Page 3</a></body></html>';
+                    $html = '<html><body><a href="/page_3.html">Page 3</a><a href="http://external/">External</a></body></html>';
                     break;
                 case 3:
-                    $html = '<html><body><a href="/page_4.html">Page 4</a></body></html>';
+                    $html = '<html><body><a href="/page_4.html">Page 4</a><a href="mailto:foo@bar.com">Invalid</a></body></html>';
                     break;
                 default:
-                    $html = '<html></html>';
+                    $html = '<html><body><a href="/page_1.html">Page 1</a></body></html>';
                     break;
             }
 
             return new DomCrawler($html, 'http://my-test');
         });
 
-        $crawler = new Crawler($client);
-
-        foreach ($crawler->crawl('http://my-test') as $page) {
-        }
-
-        $this->assertCount(5, $crawler->getUrlsCrawled());
+        return $client;
     }
 
     protected function tearDown()
