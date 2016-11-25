f014119facbc7e782871f38277ee42eb3bae5896
Robert <robertslootjes@gmail.com>
2016-11-25 12:07:09 +0100

    - Added basic crawler tests

--- tests/src/CrawlerTest.php
+++ tests/src/CrawlerTest.php
@@ -5,9 +5,9 @@ namespace tests\MediaMonks\Crawler;
 use MediaMonks\Crawler\Crawler;
 use MediaMonks\Crawler\Url\Matcher\UrlMatcherInterface;
 use Mockery as m;
-use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Symfony\Component\BrowserKit\Client;
+use Symfony\Component\DomCrawler\Crawler as DomCrawler;
 
 class CrawlerTest extends \PHPUnit_Framework_TestCase
 {
@@ -51,17 +51,19 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
     {
         $logger = m::mock(NullLogger::class);
 
-        $crawler = new Crawler(null, [
-            'limit' => 1,
-            'stop_on_error' => true,
-            'logger' => $logger,
+        $crawler = new Crawler(
+            null, [
+            'limit'                  => 1,
+            'stop_on_error'          => true,
+            'logger'                 => $logger,
             'whitelist_url_matchers' => [
-                m::mock(UrlMatcherInterface::class)
+                m::mock(UrlMatcherInterface::class),
             ],
             'blacklist_url_matchers' => [
-                m::mock(UrlMatcherInterface::class)
-            ]
-        ]);
+                m::mock(UrlMatcherInterface::class),
+            ],
+        ]
+        );
 
         $this->assertEquals(1, $crawler->getLimit());
         $this->assertTrue($crawler->getStopOnError());
@@ -70,5 +72,58 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(1, $crawler->getBlacklistUrlMatchers());
     }
 
+    public function test_crawl_single_page()
+    {
+        $domCrawler = new DomCrawler('<html></html>');
 
+        $client = m::mock(Client::class);
+        $client->shouldReceive('request')->once()->andReturn($domCrawler);
+
+        $crawler = new Crawler($client);
+
+        foreach ($crawler->crawl('http://my-test') as $page) {
+        }
+
+        $this->assertCount(1, $crawler->getUrlsCrawled());
+    }
+
+    public function test_crawl_multiple_pages()
+    {
+        $client = m::mock(Client::class);
+
+        $i = 0;
+        $client->shouldReceive('request')->andReturnUsing(function() use(&$i) {
+            $i++;
+            switch($i) {
+                case 1:
+                    $html = '<html><body><a href="/page_1.html">Page 1</a><a href="/page_2.html">Page 2</a></body></html>';
+                    break;
+                case 2:
+                    $html = '<html><body><a href="/page_3.html">Page 3</a></body></html>';
+                    break;
+                case 3:
+                    $html = '<html><body><a href="/page_4.html">Page 4</a></body></html>';
+                    break;
+                default:
+                    $html = '<html></html>';
+                    break;
+            }
+
+            return new DomCrawler($html, 'http://my-test');
+        });
+
+        $crawler = new Crawler($client);
+
+        foreach ($crawler->crawl('http://my-test') as $page) {
+        }
+
+        $this->assertCount(5, $crawler->getUrlsCrawled());
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        m::close();
+    }
 }
